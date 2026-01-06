7e9f29197d2a00aef1328e801be114020e621f64
Anantha Medu <anantha_medu@Ananthas-MacBook-Air.local>
2026-01-07 01:02:02 +0530

    Added Logging framework

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import utils.Log;
 
 import java.time.Duration;
 
@@ -15,8 +16,10 @@ public class BaseTest
     @BeforeMethod
     public void setup()
     {
+        Log.info("Setting up driver");
         driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
+        Log.info("Opened browser and url");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
 
     }
@@ -26,6 +29,7 @@ public class BaseTest
     {
         if(driver != null)
         {
+            Log.info("CLosing the browser..");
             driver.quit();
         }
     }
